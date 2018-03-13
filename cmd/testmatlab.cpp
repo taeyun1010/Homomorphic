1e585c3539d6cb8c919200505a0b49b38dff2595
Taeyun Kim <taeyun1010@yahoo.ca>
2018-03-13 10:05:04 +0900

    some files to compile mex

---
+++ cmd/testmatlab.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+//#include "../FHEW.h"
+//#include "common.h"
+
+#include <iostream>
+//#include <cstdlib>
+//#include <cassert>
+
+using namespace std;
+
+
+
+int main(int argc, char *argv[]) {
+
+
+
+  cout << "input1 bits = \n";
+ 
+
+
+  //TODO
+  //perform homomorphic addition
+  //FHEW::HomGate(ct3, AND, *EK,*ct1,*ct2);
+
+  //SaveCipherText(ct3,oct_fn);
+}
