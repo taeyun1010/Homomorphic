a67a6e53f6d47c0725bbd4aa4ac87f184434670b
taeyun1010 <taeyun1010@yahoo.ca>
2018-03-17 18:31:41 +0900

    Add files via upload

--- cmd/addBit.cpp
+++ cmd/addBit.cpp
@@ -7,13 +7,16 @@
 #include <cstdlib>
 #include <cassert>
 
+#include <ctime>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 FHEW::EvalKey* EK;
 
 LWE::SecretKey* SK;
 
-int numBits =32;
+int numBits =8;
 
 void XorGate(LWE::CipherText* res, const FHEW::EvalKey& EK, const LWE::CipherText& ct1, const LWE::CipherText& ct2) {
     LWE::CipherText *orResult, *nandResult;
@@ -200,13 +203,115 @@ void subtractBit(LWE::CipherText* difference, LWE::CipherText* bout, const FHEW:
  
 }
 
+// compute ct1[] - ct2[]
+void subtraction(LWE::CipherText* result[], int* borrowOutput, const FHEW::EvalKey& EK, LWE::CipherText* ct1[], LWE::CipherText* ct2[], LWE::CipherText& initialborrow){
+
+    LWE::CipherText *borrowResult, *borrow;
+    borrowResult = new LWE::CipherText;
+
+    borrow = new LWE::CipherText;
+    
+  
+    
+    //sumResult = new LWE::CipherText;
+    for(int i=0; i<numBits; i++){
+        LWE::CipherText* inputbit1 = ct1[i];
+        LWE::CipherText* inputbit2 = ct2[i];
+       int inputonebit = LWE::Decrypt(*SK, *inputbit1);
+      
+      int inputonebit2 = LWE::Decrypt(*SK, *inputbit2);
+      //cout << i <<"th inputbit for array1 " << inputonebit << "\n";
+      
+      //cout << i <<"th inputbit for array2 " << inputonebit2 << "\n";
+        LWE::CipherText *thissubResult;
+        thissubResult = new LWE::CipherText;
+        if (i==0){
+            
+            subtractBit(thissubResult, borrowResult, EK, *inputbit1, *inputbit2, initialborrow);
+             //int sumresultbit = LWE::Decrypt(*SK, *sumResult);
+             
+             //cout << "0th sum bit = " << sumresultbit << "\n";
+        }
+        else{
+            subtractBit(thissubResult, borrowResult, EK, *inputbit1, *inputbit2, *borrow);
+        }
+        result[i] = thissubResult;
+        borrow = borrowResult;
+    }
+}
+
+//least significant ones first
+vector<int> convert(int x) {
+  vector<int> ret;
+  while(x) {
+    if (x&1)
+      ret.push_back(1);
+    else
+      ret.push_back(0);
+    x>>=1;  
+  }
+  
+  while(!(ret.size() >= numBits)){
+    ret.push_back(0);
+  }
+  
+  //reverse(ret.begin(),ret.end());
+  return ret;
+}
+
+void printvector(vector<int> bits){
+    for (vector<int>::const_iterator i = bits.begin(); i != bits.end(); ++i)
+    cout << *i << ' ';
+}
+
+void encryptInt(LWE::CipherText* result[], int input){
+    
+    vector<int> bits = convert(input);
+    
+    int j=0;
+    for (vector<int>::const_iterator i = bits.begin(); i != bits.end(); ++i){
+        cout << *i << ' ';
+        LWE::CipherText* ct = new LWE::CipherText;
+        LWE::Encrypt(ct, *SK, *i);
+        result[j] = ct;
+        j++;
+    }
+    
+    
+}
+// 
+// void decryptInt(LWE::CipherText* result[], int input){
+//     
+//     int j=0;
+//     for (vector<int>::const_iterator i = bits.begin(); i != bits.end(); ++i){
+//         cout << *i << ' ';
+//         LWE::CipherText* ct = new LWE::CipherText;
+//         LWE::Encrypt(ct, *SK, *i);
+//         result[j] = ct;
+//         j++;
+//     }
+//     
+//     
+// }
+
+// TODO: encrypts two inputs, adds them, decrypts to see if addition works
+void addPlaintext(int input1, int input2){
+    vector<int> bits1 = convert(input1);
+    vector<int> bits2 = convert(input2);
+    
+    for (vector<int>::const_iterator i = bits1.begin(); i != bits1.end(); ++i)
+    cout << *i << ' ';
+}
+
+
 int main(int argc, char *argv[]) {
 
   char *EKfilename = "ev.key";
   char *SKfilename = "sec.key";
   
-
-
+  int input1 = atoi(argv[1]);
+  int input2 = atoi(argv[2]);
+  
   FHEW::Setup();
 
   SK = LoadSecretKey(SKfilename);
@@ -233,31 +338,78 @@ int main(int argc, char *argv[]) {
   sumResult = new LWE::CipherText;
   
   //initialize input
-  LWE::Encrypt(ct1, *SK, 1);
-  LWE::Encrypt(ct2, *SK, 0);
-   LWE::Encrypt(ct3, *SK, 0);
-  LWE::Encrypt(ct4, *SK, 1);
+  LWE::Encrypt(ct1, *SK, 0);
+  LWE::Encrypt(ct2, *SK, 1);
+   LWE::Encrypt(ct3, *SK, 1);
+  LWE::Encrypt(ct4, *SK, 0);
   LWE::Encrypt(initialbin, *SK, 0);
   //LWE::Encrypt(carry, *SK, 1);
   
   //initialize carry to 0 
   LWE::Encrypt(initialcarry, *SK, 0);
   
+  //
+  //test addPlaintext
+  //cout << "input1 = " << input1 << "\n";
+  addPlaintext(input1,input2);
+  //
+  //
+  
+//     //
+//   //test subtraction
+//   int carryOutput = 0;
+//   int* carryPointer = &carryOutput;
+//   
+//   //4 byte integer
+//   LWE::CipherText* sum[numBits];
+//   LWE::CipherText* ctarray1[numBits];
+//   LWE::CipherText* ctarray2[numBits];
+//   
+//   ctarray1[0] = ct1;
+//   ctarray2[0] = ct2;
+//   ctarray1[1] = ct3;
+//   ctarray2[1] = ct4;
+//   for (int i=2; i<numBits; i++){
+//       LWE::CipherText *newct1,*newct2;
+//   
+//       newct1 = new LWE::CipherText;
+//       newct2 = new LWE::CipherText;
+//       LWE::Encrypt(newct1, *SK, 0);
+//       LWE::Encrypt(newct2, *SK, 0);
+//       ctarray1[i] = newct1;
+//       ctarray2[i] = newct2;
+//   }
+//   
+//   clock_t begin = clock();
+//   
+//   subtraction(sum, carryPointer, *EK, ctarray1, ctarray2, *initialcarry);
+//   
+//   clock_t end = clock();
+//   double elapsed_secs = double(end-begin) / CLOCKS_PER_SEC;
+//   cout << "elapsed sec = " << elapsed_secs << "\n";
+//   for (int i=0; i<numBits; i++){
+//       LWE::CipherText* onebitsum = sum[i];
+//       int outputonebit = LWE::Decrypt(*SK, *onebitsum);
+//       cout << i <<"th outputbit " << outputonebit << "\n";
+//   }
+//   //
+//   //
+  
   
   // test addBit
   //addBit(sumResult, carryResult, *EK, *ct1, *ct2, *carry);
   //
   
-  //
-  // test subtractBit 
-  subtractBit(difference, bout, *EK, *ct1, *ct2, *initialbin);
-  int outputdifference = LWE::Decrypt(*SK, *difference);
-  int outputbout = LWE::Decrypt(*SK, *bout);
-  cout << "outputdifference = " << outputdifference << "\n";
-  cout << "outputbout = " << outputbout << "\n";
-  //
-  //
-  
+//   //
+//   // test subtractBit 
+//   subtractBit(difference, bout, *EK, *ct1, *ct2, *initialbin);
+//   int outputdifference = LWE::Decrypt(*SK, *difference);
+//   int outputbout = LWE::Decrypt(*SK, *bout);
+//   cout << "outputdifference = " << outputdifference << "\n";
+//   cout << "outputbout = " << outputbout << "\n";
+//   //
+//   //
+//   
 //   //
 //   //test addition
 //   int carryOutput = 0;
@@ -282,33 +434,42 @@ int main(int argc, char *argv[]) {
 //       ctarray1[i] = newct1;
 //       ctarray2[i] = newct2;
 //   }
+//   
+//   clock_t begin = clock();
+//   
 //   addition(sum, carryPointer, *EK, ctarray1, ctarray2, *initialcarry);
 //   
+//   clock_t end = clock();
+//   double elapsed_secs = double(end-begin) / CLOCKS_PER_SEC;
+//   cout << "elapsed sec = " << elapsed_secs << "\n";
 //   for (int i=0; i<numBits; i++){
 //       LWE::CipherText* onebitsum = sum[i];
 //       int outputonebit = LWE::Decrypt(*SK, *onebitsum);
 //       cout << i <<"th outputbit " << outputonebit << "\n";
 //   }
-//   
-// //   for (int i=0; i<numBits; i++){
-// //   
-// //       LWE::CipherText* input1ct;
-// //       LWE::CipherText* input2ct;
-// //       input1ct = ctarray1[i];
-// //       input2ct =  ctarray2[i];
-// //       
-// //       
-// //       
-// //       int inputonebit = LWE::Decrypt(*SK, *input1ct);
-// //       
-// //       int inputonebit2 = LWE::Decrypt(*SK, *input2ct);
-// //       cout << i <<"th inputbit for array1 " << inputonebit << "\n";
-// //       
-// //       cout << i <<"th inputbit for array2 " << inputonebit2 << "\n";
-// //   }
-//   
 //   //
 //   //
+  
+  
+//   for (int i=0; i<numBits; i++){
+//   
+//       LWE::CipherText* input1ct;
+//       LWE::CipherText* input2ct;
+//       input1ct = ctarray1[i];
+//       input2ct =  ctarray2[i];
+//       
+//       
+//       
+//       int inputonebit = LWE::Decrypt(*SK, *input1ct);
+//       
+//       int inputonebit2 = LWE::Decrypt(*SK, *input2ct);
+//       cout << i <<"th inputbit for array1 " << inputonebit << "\n";
+//       
+//       cout << i <<"th inputbit for array2 " << inputonebit2 << "\n";
+//   }
+  
+  //
+  //
   
   
 //   int outputsum = LWE::Decrypt(*SK, *sumResult);
