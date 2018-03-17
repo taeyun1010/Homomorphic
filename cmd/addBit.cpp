8ce89ee994be851271303368b90385a7b02ea508
taeyun1010 <taeyun1010@yahoo.ca>
2018-03-17 12:46:09 +0900

    Add files via upload

--- cmd/addBit.cpp
+++ cmd/addBit.cpp
@@ -11,28 +11,31 @@ using namespace std;
 
 FHEW::EvalKey* EK;
 
-// void XorGate(LWE::CipherText* res, const FHEW::EvalKey& EK, const LWE::CipherText& ct1, const LWE::CipherText& ct2) {
-//     LWE::CipherText *orResult, *nandResult;
-//     
-//     orResult = new LWE::CipherText;
-//     nandResult = new LWE::CipherText;
-//     
-//     
-//     BinGate gate;
-//     gate = OR;
-//     
-//     FHEW::HomGate(orResult, gate, EK,ct1,ct2);
-//     
-//     gate = NAND;
-//     
-//     FHEW::HomGate(nandResult, gate, EK,ct1,ct2);
-//     
-//     gate = AND;
-//     
-//     FHEW::HomGate(res, gate, EK,*orResult,*nandResult);
-//     
-// }
-
+void XorGate(LWE::CipherText* res, const FHEW::EvalKey& EK, const LWE::CipherText& ct1, const LWE::CipherText& ct2) {
+    LWE::CipherText *orResult, *nandResult;
+    
+    orResult = new LWE::CipherText;
+    nandResult = new LWE::CipherText;
+    
+    
+    BinGate gate;
+    gate = OR;
+    
+    FHEW::HomGate(orResult, gate, EK,ct1,ct2);
+    
+    gate = NAND;
+    
+    FHEW::HomGate(nandResult, gate, EK,ct1,ct2);
+    
+    gate = AND;
+    
+    FHEW::HomGate(res, gate, EK,*orResult,*nandResult);
+    
+}
+void addBit(LWE::CipherText* res, const FHEW::EvalKey& EK, const LWE::CipherText& ct1, const LWE::CipherText& ct2){
+    
+    
+}
 
 int main(int argc, char *argv[]) {
 
@@ -47,7 +50,7 @@ int main(int argc, char *argv[]) {
   EK = LoadEvalKey(EKfilename);
 
   LWE::CipherText *ct1,*ct2,*carry, 
-          *orResult, *andResult, *nandResult, 
+         *andResult,  
           *ct1xorct2Result, *ct1xorct2orcarryResult, 
           *ct1xorct2nandcarryResult, *sumResult, *ct1xorct2andcarryResult,
           *carryResult;
@@ -58,8 +61,8 @@ int main(int argc, char *argv[]) {
   ct1 = new LWE::CipherText;
   ct2 = new LWE::CipherText;
   carry = new LWE::CipherText;
-  orResult = new LWE::CipherText;
-  nandResult = new LWE::CipherText;
+  //orResult = new LWE::CipherText;
+  //nandResult = new LWE::CipherText;
   andResult = new LWE::CipherText;
   ct1xorct2Result = new LWE::CipherText;
   ct1xorct2andcarryResult = new LWE::CipherText;
@@ -74,19 +77,26 @@ int main(int argc, char *argv[]) {
   LWE::Encrypt(ct2, *SK, 0);
   LWE::Encrypt(carry, *SK, 1);
   
-  // ct1 XOR ct2
-  gate = OR;
+//   // ct1 XOR ct2
+//   gate = OR;
+//   
+//   FHEW::HomGate(orResult, gate, *EK,*ct1,*ct2);
+//   
+//   gate = NAND;
+//   
+//   FHEW::HomGate(nandResult, gate, *EK,*ct1,*ct2);
+//   
+//   gate = AND;
+//   
+//   FHEW::HomGate(ct1xorct2Result, gate, *EK,*orResult,*nandResult);
+//   //
   
-  FHEW::HomGate(orResult, gate, *EK,*ct1,*ct2);
+  XorGate(ct1xorct2Result, *EK, *ct1, *ct2);
   
-  gate = NAND;
+//   int xorResult = LWE::Decrypt(*SK, *ct1xorct2Result);
+//   cout << "result = " << xorResult << "\n";
   
-  FHEW::HomGate(nandResult, gate, *EK,*ct1,*ct2);
   
-  gate = AND;
-  
-  FHEW::HomGate(ct1xorct2Result, gate, *EK,*orResult,*nandResult);
-  //
   
 //   XorGate(ct1xorct2Result, *EK, *orResult, *nandResult);
 
