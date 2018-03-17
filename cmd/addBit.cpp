964d41366c061a6925fffbc8d4bdd41dea11403d
taeyun1010 <taeyun1010@yahoo.ca>
2018-03-17 16:54:59 +0900

    subtract bit

--- cmd/addBit.cpp
+++ cmd/addBit.cpp
@@ -110,6 +110,7 @@ void addBit(LWE::CipherText* sum, LWE::CipherText* carry, const FHEW::EvalKey& E
     
 }
 
+
 void addition(LWE::CipherText* sum[], int* carryOutput, const FHEW::EvalKey& EK, LWE::CipherText* ct1[], LWE::CipherText* ct2[], LWE::CipherText& initialcarry){
 
     LWE::CipherText *carryResult, *carry;
@@ -146,6 +147,59 @@ void addition(LWE::CipherText* sum[], int* carryOutput, const FHEW::EvalKey& EK,
     }
 }
 
+// calculates ct1 - ct2
+void subtractBit(LWE::CipherText* difference, LWE::CipherText* bout, const FHEW::EvalKey& EK, const LWE::CipherText& ct1, const LWE::CipherText& ct2, const LWE::CipherText& bin){
+    // ~ct1
+    LWE::CipherText *notct1Result = new LWE::CipherText;
+    // (~ct1) AND ct2
+    LWE::CipherText *notct1andct2Result = new LWE::CipherText;
+    // ct1 XOR ct2
+    LWE::CipherText *ct1xorct2Result = new LWE::CipherText;
+//     // ct1 XOR ct2 XOR bin
+//     LWE::CipherText *diffResult = new LWE::CipherText;
+    // ~(ct1 XOR ct2)
+    LWE::CipherText *notct1xorct2Result = new LWE::CipherText;
+    // ~(ct1 XOR ct2) AND bin
+    LWE::CipherText *notct1xorct2andbinResult = new LWE::CipherText;
+//     // (~(ct1 XOR ct2) AND bin) OR ((~ct1) AND ct2)
+//     LWE::CipherText *boutResult = new LWE::CipherText;
+    
+    
+    BinGate gate;
+   
+    
+  FHEW::HomNOT(notct1Result, ct1);
+  //
+  
+  gate = AND;
+  
+  FHEW::HomGate(notct1andct2Result, gate, EK,*notct1Result,ct2);
+  
+  XorGate(ct1xorct2Result, EK, ct1, ct2);
+  
+  int ct1xorct2 = LWE::Decrypt(*SK, *ct1xorct2Result);
+//   cout << "ct1xorct2 = " << ct1xorct2 << "\n";
+  
+  XorGate(difference, EK, *ct1xorct2Result, bin);
+   int binvalue = LWE::Decrypt(*SK, bin);
+//   cout << "binvalue = " << binvalue << "\n";
+  
+  
+  FHEW::HomNOT(notct1xorct2Result, *ct1xorct2Result);
+  
+  FHEW::HomGate(notct1xorct2andbinResult, gate, EK,*notct1xorct2Result, bin);
+  
+  gate = OR;
+  FHEW::HomGate(bout, gate, EK, *notct1xorct2andbinResult, *notct1andct2Result);
+  
+  int outputdifference = LWE::Decrypt(*SK, *difference);
+  int outputbout = LWE::Decrypt(*SK, *bout);
+//   cout << "outputdifference = " << outputdifference << "\n";
+//   cout << "outputbout = " << outputbout << "\n";
+  
+ 
+}
+
 int main(int argc, char *argv[]) {
 
   char *EKfilename = "ev.key";
@@ -160,7 +214,7 @@ int main(int argc, char *argv[]) {
 
   LWE::CipherText *ct1,*ct2,*ct3, *ct4, *carry, *initialcarry, 
            *sumResult, 
-          *carryResult;
+          *carryResult, *initialbin, *difference, *bout;
 
   
   
@@ -170,7 +224,9 @@ int main(int argc, char *argv[]) {
   ct4 = new LWE::CipherText;
   carry = new LWE::CipherText;
   initialcarry = new LWE::CipherText;
-
+  initialbin = new LWE::CipherText;
+  difference = new LWE::CipherText;
+  bout = new LWE::CipherText;
 
   carryResult = new LWE::CipherText;
 
@@ -178,9 +234,10 @@ int main(int argc, char *argv[]) {
   
   //initialize input
   LWE::Encrypt(ct1, *SK, 1);
-  LWE::Encrypt(ct2, *SK, 1);
+  LWE::Encrypt(ct2, *SK, 0);
    LWE::Encrypt(ct3, *SK, 0);
   LWE::Encrypt(ct4, *SK, 1);
+  LWE::Encrypt(initialbin, *SK, 0);
   //LWE::Encrypt(carry, *SK, 1);
   
   //initialize carry to 0 
@@ -192,56 +249,66 @@ int main(int argc, char *argv[]) {
   //
   
   //
-  //test addition
-  int carryOutput = 0;
-  int* carryPointer = &carryOutput;
+  // test subtractBit 
+  subtractBit(difference, bout, *EK, *ct1, *ct2, *initialbin);
+  int outputdifference = LWE::Decrypt(*SK, *difference);
+  int outputbout = LWE::Decrypt(*SK, *bout);
+  cout << "outputdifference = " << outputdifference << "\n";
+  cout << "outputbout = " << outputbout << "\n";
+  //
+  //
   
-  //4 byte integer
-  LWE::CipherText* sum[numBits];
-  LWE::CipherText* ctarray1[numBits];
-  LWE::CipherText* ctarray2[numBits];
-  
-  ctarray1[0] = ct1;
-  ctarray2[0] = ct2;
-  ctarray1[1] = ct3;
-  ctarray2[1] = ct4;
-  for (int i=2; i<numBits; i++){
-      LWE::CipherText *newct1,*newct2;
-  
-      newct1 = new LWE::CipherText;
-      newct2 = new LWE::CipherText;
-      LWE::Encrypt(newct1, *SK, 0);
-      LWE::Encrypt(newct2, *SK, 0);
-      ctarray1[i] = newct1;
-      ctarray2[i] = newct2;
-  }
-  addition(sum, carryPointer, *EK, ctarray1, ctarray2, *initialcarry);
-  
-  for (int i=0; i<numBits; i++){
-      LWE::CipherText* onebitsum = sum[i];
-      int outputonebit = LWE::Decrypt(*SK, *onebitsum);
-      cout << i <<"th outputbit " << outputonebit << "\n";
-  }
-  
-//   for (int i=0; i<numBits; i++){
+//   //
+//   //test addition
+//   int carryOutput = 0;
+//   int* carryPointer = &carryOutput;
 //   
-//       LWE::CipherText* input1ct;
-//       LWE::CipherText* input2ct;
-//       input1ct = ctarray1[i];
-//       input2ct =  ctarray2[i];
-//       
-//       
-//       
-//       int inputonebit = LWE::Decrypt(*SK, *input1ct);
-//       
-//       int inputonebit2 = LWE::Decrypt(*SK, *input2ct);
-//       cout << i <<"th inputbit for array1 " << inputonebit << "\n";
-//       
-//       cout << i <<"th inputbit for array2 " << inputonebit2 << "\n";
+//   //4 byte integer
+//   LWE::CipherText* sum[numBits];
+//   LWE::CipherText* ctarray1[numBits];
+//   LWE::CipherText* ctarray2[numBits];
+//   
+//   ctarray1[0] = ct1;
+//   ctarray2[0] = ct2;
+//   ctarray1[1] = ct3;
+//   ctarray2[1] = ct4;
+//   for (int i=2; i<numBits; i++){
+//       LWE::CipherText *newct1,*newct2;
+//   
+//       newct1 = new LWE::CipherText;
+//       newct2 = new LWE::CipherText;
+//       LWE::Encrypt(newct1, *SK, 0);
+//       LWE::Encrypt(newct2, *SK, 0);
+//       ctarray1[i] = newct1;
+//       ctarray2[i] = newct2;
 //   }
-  
-  //
-  //
+//   addition(sum, carryPointer, *EK, ctarray1, ctarray2, *initialcarry);
+//   
+//   for (int i=0; i<numBits; i++){
+//       LWE::CipherText* onebitsum = sum[i];
+//       int outputonebit = LWE::Decrypt(*SK, *onebitsum);
+//       cout << i <<"th outputbit " << outputonebit << "\n";
+//   }
+//   
+// //   for (int i=0; i<numBits; i++){
+// //   
+// //       LWE::CipherText* input1ct;
+// //       LWE::CipherText* input2ct;
+// //       input1ct = ctarray1[i];
+// //       input2ct =  ctarray2[i];
+// //       
+// //       
+// //       
+// //       int inputonebit = LWE::Decrypt(*SK, *input1ct);
+// //       
+// //       int inputonebit2 = LWE::Decrypt(*SK, *input2ct);
+// //       cout << i <<"th inputbit for array1 " << inputonebit << "\n";
+// //       
+// //       cout << i <<"th inputbit for array2 " << inputonebit2 << "\n";
+// //   }
+//   
+//   //
+//   //
   
   
 //   int outputsum = LWE::Decrypt(*SK, *sumResult);
